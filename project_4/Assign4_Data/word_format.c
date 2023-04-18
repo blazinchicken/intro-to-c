3f6fc99a8289537ee8058186333429041b010d12
blazinchicken <kyblazinchicken@gmail.com>
2023-04-18 13:59:38 -0500

    start of hell

---
+++ project_4/Assign4_Data/word_format.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+/*
+To-Do List
+ 1. Open given file
+    i. command line arg 2
+ 2. Take in file, scan in 1 of 2 ways
+    i. use fgets & strtok to get lines, then sort for words
+    ii. use fscanf to scan lines directly
+ 3. *part 1* Take scanned words, format into a file with length = command line arg 1
+    i. doesnt end with space, ends with new line
+    ii. formats into "normal" looking text
+ 4. *part 2* Takes instances of words, sorts them, and displays number of times that instance is found
+    i. use qsort to stort
+    ii. needs to have some sort of counter, variable or addition of times idk. 
+ 5. these are in 2 seperate output files, one is .out, other is .words
+*/
+
+
+
+int main(){
+
+   return 0;
+}
